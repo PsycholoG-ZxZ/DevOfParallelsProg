6a9cb76f428d53f9248734c0f7ed21bef9586cfa
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-10-26 00:26:26 +0300

    Scripted auto-commit on change (2019-10-26 00:26:26) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/Parser.java
+++ Lab1_AirportsTable/src/main/java/Parser.java
@@ -1,7 +1,7 @@
 import org.apache.hadoop.io.Text;
 
 public class Parser {
-    public static String[] parseIdDescr(Text str){
+    public static String[] parseIdDescr(Text str) {
         String[] data = str.toString().split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
         return data;
     }
