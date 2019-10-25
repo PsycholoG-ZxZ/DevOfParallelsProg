135bcffaa53aa62927b4a4f2a11895975a67a8f7
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-10-26 02:32:43 +0300

    Scripted auto-commit on change (2019-10-26 02:32:43) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/AirportMapper.java
+++ Lab1_AirportsTable/src/main/java/AirportMapper.java
@@ -1,3 +1,4 @@
+import com.sun.org.apache.xalan.internal.xsltc.dom.KeyIndex;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -5,14 +6,14 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class AirportMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
+public class AirportMapper extends Mapper<LongWritable, Text, KeyIDnBase, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,InterruptedException {
         String data[] =Parser.parseIdDescr(value);
         String id = data[0];
         KeyIDnBase aKey = new KeyIDnBase(id, 0);
         Text title = new Text(data[1]);
-        context.write(aKey,);
+        context.write(aKey,title);
 
 
     }
