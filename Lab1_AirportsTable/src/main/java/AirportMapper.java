710704ccf7622a7cdc52dca19a5e6ea89358e3a2
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-10-26 01:15:00 +0300

    Scripted auto-commit on change (2019-10-26 01:15:00) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/AirportMapper.java
+++ Lab1_AirportsTable/src/main/java/AirportMapper.java
@@ -1,2 +1,5 @@
-public class AirportMapper extends Mapper<> {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class AirportMapper extends Mapper<LongWritable> {
 }
