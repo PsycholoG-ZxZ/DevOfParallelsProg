af75d3e57b5a84f1d552b26f624566dfe940154c
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-10-26 02:02:04 +0300

    Scripted auto-commit on change (2019-10-26 02:02:04) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/KeyIDnBase.java
+++ Lab1_AirportsTable/src/main/java/KeyIDnBase.java
@@ -3,6 +3,7 @@ import org.apache.hadoop.io.WritableComparable;
 public class KeyIDnBase implements WritableComparable<KeyIDnBase> {
     public int aeroport_id;
     public int base;
-    
+
+    public KeyIDnBase
 
 }
