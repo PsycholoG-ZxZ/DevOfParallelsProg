6414f579305689ccbc7f6f0089f5856675f03c23
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-10-25 23:37:33 +0300

    Scripted auto-commit on change (2019-10-25 23:37:33) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/ConnectingDelaysFlights.java
+++ Lab1_AirportsTable/src/main/java/ConnectingDelaysFlights.java
@@ -9,7 +9,8 @@ public class ConnectingDelaysFlights{
         if (args.length != 2) System.exit(-1);
 
         Job job = Job.getInstance();
-        job.setJarByClass();
-        job.setJobName("Word count");
+        job.setJarByClass(ConnectingDelaysFlights.class);
+        job.setJobName("Connecting");
+        
     }
 }
