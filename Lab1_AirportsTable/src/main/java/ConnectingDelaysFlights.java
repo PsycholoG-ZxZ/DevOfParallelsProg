05d224990026eeace2c6084baba827094fc39605
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-10-25 21:05:22 +0300

    Scripted auto-commit on change (2019-10-25 21:05:22) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/ConnectingDelaysFlights.java
+++ Lab1_AirportsTable/src/main/java/ConnectingDelaysFlights.java
@@ -1,8 +1,3 @@
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
-
 public class ConnectingDelaysFlights{
     public static void main (String[] args) throws Exception {
         if (args.length != 2) System.exit(-1);
