d64587d758d81a932018d789a0e2135a5884bd49
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-10-26 02:03:06 +0300

    Scripted auto-commit on change (2019-10-26 02:03:06) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/KeyIDnBase.java
+++ Lab1_AirportsTable/src/main/java/KeyIDnBase.java
@@ -5,7 +5,12 @@ public class KeyIDnBase implements WritableComparable<KeyIDnBase> {
     public int base;
 
     public KeyIDnBase(){
-        this.aeropory_id = new int 
+        this.aeroport_id = new int();
+        this.base = new int();
+    }
+    public KeyIDnBase(int id, int ){
+        this.aeroport_id = new int();
+        this.base = new int();
     }
 
 }
