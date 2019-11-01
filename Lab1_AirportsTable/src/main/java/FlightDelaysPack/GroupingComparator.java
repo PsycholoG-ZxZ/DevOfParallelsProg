812bb17abcf098c08ea5e87a67715f757d713bb9
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-02 01:55:38 +0300

    Scripted auto-commit on change (2019-11-02 01:55:38) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/FlightDelaysPack/GroupingComparator.java
+++ Lab1_AirportsTable/src/main/java/FlightDelaysPack/GroupingComparator.java
@@ -11,10 +11,6 @@ public class GroupingComparator extends WritableComparator{
     public int compare (WritableComparable ComparablePairFrst, WritableComparable ComparablePairSec){
         KeyIDnBase CP_Frst = (KeyIDnBase) ComparablePairFrst;
         KeyIDnBase CP_Sec = (KeyIDnBase) ComparablePairSec;
-        //return CP_Frst.getAeroport_id().compareTo(CP_Sec.getAeroport_id());
-        if (CP_Frst.aeroport_id == CP_Sec.getAeroport_id()) return 0;
-        if (CP_Frst.aeroport_id > CP_Sec.getAeroport_id()) return 1;
-        if (CP_Frst.aeroport_id < CP_Sec.getAeroport_id()) return -1;
-        return 0;
+        return CP_Frst.getAeroport_id().compareTo(CP_Sec.getAeroport_id());
     }
 }
