51be3dacf1112678878f7a4834852e03410b1d06
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-02 01:58:03 +0300

    Scripted auto-commit on change (2019-11-02 01:58:03) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/FlightDelaysPack/KeyIDnBase.java
+++ Lab1_AirportsTable/src/main/java/FlightDelaysPack/KeyIDnBase.java
@@ -1,5 +1,4 @@
 package FlightDelaysPack;
-
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.WritableComparable;
 
@@ -8,69 +7,42 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class KeyIDnBase implements WritableComparable<KeyIDnBase> {
-    //private IntWritable aeroport_id;
-    //private IntWritable base;
-    int aeroport_id;
-    int base;
+    private IntWritable aeroport_id;
+    private IntWritable base;
 
     public KeyIDnBase(){
-    //    this.aeroport_id = new IntWritable();
-    //    this.base = new IntWritable();
-
+        this.aeroport_id = new IntWritable();
+        this.base = new IntWritable();
     }
     public KeyIDnBase(String id, int baseOut){
-        //this.aeroport_id = new IntWritable(Integer.parseInt(id.replace("\"", "")));
-        //this.base = new IntWritable( baseOut );
-        aeroport_id = Integer.parseInt(id);
-        base = baseOut;
+        this.aeroport_id = new IntWritable(Integer.parseInt(id.replace("\"", "")));
+        this.base = new IntWritable( baseOut );
     }
 
-    public int getAeroport_id(){
+    public IntWritable getAeroport_id(){
         return aeroport_id;
     }
 
-    public int getBase(){
+    public IntWritable getBase(){
         return base;
     }
 
     public void readFields(DataInput in) throws IOException{
-      //  aeroport_id.readFields(in);
-       // base.readFields(in);
-        aeroport_id = in.readInt();
-        base = in.readInt();
+        aeroport_id.readFields(in);
+        base.readFields(in);
     }
 
     public void write (DataOutput out) throws IOException{
-        //aeroport_id.write(out);
-        //base.write(out);
-        out.writeInt(aeroport_id);
-        out.writeInt(base);
+        aeroport_id.write(out);
+        base.write(out);
     }
 
     public int compareTo(KeyIDnBase keyNeitr) {
-        //int act = aeroport_id.compareTo(keyNeitr.getAeroport_id());
-        int act = 0;
-        if (aeroport_id== keyNeitr.getAeroport_id()){
-            act = 0;
-        }
-        if (aeroport_id > keyNeitr.getAeroport_id()){
-            act = 1;
-        }
-        if (aeroport_id < keyNeitr.getAeroport_id()){
-            act = -1;
-        }
-
+        int act = aeroport_id.compareTo(keyNeitr.getAeroport_id());
         if (act != 0)
             return act;
-        //return base.compareTo(keyNeitr.getBase());
-        if (base == keyNeitr.getBase()){
-            return 0;
-        }else{
-            if (base > keyNeitr.getBase()){
-                return 1;
-            }else{
-                return -1;
-            }
-        }
+        return base.compareTo(keyNeitr.getBase());
+
     }
+
 }
