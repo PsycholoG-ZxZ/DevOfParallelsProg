5a1dd8567bf3af12f60a95bb2c60898abd90870c
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-01 20:08:04 +0300

    Scripted auto-commit on change (2019-11-01 20:08:04) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/MyReducer.java
+++ Lab1_AirportsTable/src/main/java/MyReducer.java
@@ -1,5 +1,7 @@
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class MyReducer extends Reducer <LongWritable, Text, KeyIDnBase, Text>{
+    
 }
