0c2aacf268b9c5f7456c5e8d81c2ffe8b21fde7c
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-02 00:10:07 +0300

    Scripted auto-commit on change (2019-11-02 00:10:07) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/ConnectingDelaysFlights.java
+++ Lab1_AirportsTable/src/main/java/ConnectingDelaysFlights.java
@@ -22,7 +22,7 @@ public class ConnectingDelaysFlights{
         job.setGroupingComparatorClass(GroupingComparator.class);
         job.setPartitionerClass(MyPartitioner.class);
         job.setReducerClass(MyReducer.class);
-        
+        job.setMapOutputKeyClass();
         job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
