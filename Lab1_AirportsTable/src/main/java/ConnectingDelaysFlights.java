0cdd86123eff45cd676091afd1c89c54c4ed7e8b
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-02 00:08:25 +0300

    Scripted auto-commit on change (2019-11-02 00:08:25) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/ConnectingDelaysFlights.java
+++ Lab1_AirportsTable/src/main/java/ConnectingDelaysFlights.java
@@ -1,11 +1,11 @@
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class ConnectingDelaysFlights{
     public static void main (String[] args) throws Exception {
