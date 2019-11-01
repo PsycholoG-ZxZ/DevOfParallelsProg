67265712060eb8b46ec39c98ac3cd30d76be4a9b
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-01 19:10:29 +0300

    Scripted auto-commit on change (2019-11-01 19:10:29) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/GroupingComparator.java
+++ Lab1_AirportsTable/src/main/java/GroupingComparator.java
@@ -3,6 +3,6 @@ import org.apache.hadoop.io.WritableComparator;
 public class GroupingComparator extends WritableComparator{
     public GroupingComparator() {
         super (KeyIDnBase.class, true);
-        
     }
+    
 }
