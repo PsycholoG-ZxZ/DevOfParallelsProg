87512be24dca397a47293fde4f01ac146a986bc0
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-02 00:07:11 +0300

    Scripted auto-commit on change (2019-11-02 00:07:11) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/ConnectingDelaysFlights.java
+++ Lab1_AirportsTable/src/main/java/ConnectingDelaysFlights.java
@@ -13,7 +13,8 @@ public class ConnectingDelaysFlights{
         Job job = Job.getInstance();
         job.setJarByClass(ConnectingDelaysFlights.class);
         job.setJobName("Connecting");
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, );
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(ConnectingDelaysFlights.class);
         job.setGroupingComparatorClass(GroupingComparator.class);
