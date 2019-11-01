1978c3128d5cf0d463fe450b49003ce9fcce586d
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-02 01:46:41 +0300

    Scripted auto-commit on change (2019-11-02 01:46:41) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/FlightDelaysPack/KeyIDnBase.java
+++ Lab1_AirportsTable/src/main/java/FlightDelaysPack/KeyIDnBase.java
@@ -49,7 +49,7 @@ public class KeyIDnBase implements WritableComparable<KeyIDnBase> {
 
     public int compareTo(KeyIDnBase keyNeitr) {
         //int act = aeroport_id.compareTo(keyNeitr.getAeroport_id());
-        int act =   Integer.compare(aeroport_id, )
+        int act =   Integer.compare(aeroport_id, keyNeitr.aeroport_id);
         if (act != 0)
             return act;
         return base.compareTo(keyNeitr.getBase());
