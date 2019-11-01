25a3c9ea0172d78f245a8ee1d14cd72a557d6cac
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-02 00:04:56 +0300

    Scripted auto-commit on change (2019-11-02 00:04:56) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/ConnectingDelaysFlights.java
+++ Lab1_AirportsTable/src/main/java/ConnectingDelaysFlights.java
@@ -3,6 +3,7 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 
 public class ConnectingDelaysFlights{
     public static void main (String[] args) throws Exception {
@@ -11,7 +12,7 @@ public class ConnectingDelaysFlights{
         Job job = Job.getInstance();
         job.setJarByClass(ConnectingDelaysFlights.class);
         job.setJobName("Connecting");
-        
+        MultipleInputs.addInputPath
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(ConnectingDelaysFlights.class);
         job.setGroupingComparatorClass(GroupingComparator.class);
