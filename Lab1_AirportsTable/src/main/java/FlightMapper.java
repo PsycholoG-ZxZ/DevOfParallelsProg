de08d3f0b248e07161dc38486e05c887f49c4c53
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-01 20:01:38 +0300

    Scripted auto-commit on change (2019-11-01 20:01:38) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/FlightMapper.java
+++ Lab1_AirportsTable/src/main/java/FlightMapper.java
@@ -8,7 +8,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, KeyIDnBase, Text>  
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,InterruptedException {
         String data[] =Parser.parseIdDescr(value);
-        String id = data[0];
+        String id = data[];
         KeyIDnBase aKey = new KeyIDnBase(id, 1);
         Text del = new Text(data[1]);
         context.write(aKey,title);
