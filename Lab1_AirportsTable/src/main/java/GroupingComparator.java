666f7958b6cc4c8a16297f8e1393d2eb33656c20
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-01 19:35:39 +0300

    Scripted auto-commit on change (2019-11-01 19:35:39) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/GroupingComparator.java
+++ Lab1_AirportsTable/src/main/java/GroupingComparator.java
@@ -6,7 +6,7 @@ public class GroupingComparator extends WritableComparator{
         super (KeyIDnBase.class, true);
     }
     @Override
-    public int compare (WritableComparable frst, WritableComparable second){
+    public int compare (WritableComparable , WritableComparable second){
             return 0;
     }
 }
