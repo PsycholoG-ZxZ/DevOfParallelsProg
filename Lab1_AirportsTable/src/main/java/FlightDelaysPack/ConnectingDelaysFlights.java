25e5988a7f50dd65d797cac5ab0e8b62957e1664
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-02 01:10:29 +0300

    Scripted auto-commit on change (2019-11-02 01:10:29) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/FlightDelaysPack/ConnectingDelaysFlights.java
+++ Lab1_AirportsTable/src/main/java/FlightDelaysPack/ConnectingDelaysFlights.java
@@ -6,21 +6,23 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.conf.Configuration;
 
 import javax.security.auth.login.AppConfigurationEntry;
-import javax.security.auth.login.Configuration;
+
 
 public class ConnectingDelaysFlights{
     public static void main (String[] args) throws Exception {
         if (args.length != 3) System.exit(-1);
 
         Job job = Job.getInstance();
-        Configuration conf = new Configuration();
+        org.apache.hadoop.conf.Configuration conf = new org.apache.hadoop.conf.Configuration();
         job.setJarByClass(ConnectingDelaysFlights.class);
         job.setJobName("Connecting");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        Path outputPath = new Path(args[2]);
         job.setPartitionerClass(MyPartitioner.class);
         job.setGroupingComparatorClass(GroupingComparator.class);
         job.setReducerClass(MyReducer.class);
@@ -31,6 +33,7 @@ public class ConnectingDelaysFlights{
 
 
         job.setNumReduceTasks(2);
+        outputPath.getFileSystem(conf);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
