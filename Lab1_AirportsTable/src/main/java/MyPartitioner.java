bf376aea5846cb818ef23ca144921c5977acc120
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-01 19:18:52 +0300

    Scripted auto-commit on change (2019-11-01 19:18:52) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/MyPartitioner.java
+++ Lab1_AirportsTable/src/main/java/MyPartitioner.java
@@ -1,4 +1,5 @@
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
-public class MyPartitioner extends Partitioner <KeyIDnBase,  {
+public class MyPartitioner extends Partitioner <KeyIDnBase, Text> {
 }
