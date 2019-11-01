149d365361250f31154d76c93ba0483d689c9f85
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-01 20:10:25 +0300

    Scripted auto-commit on change (2019-11-01 20:10:25) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/MyReducer.java
+++ Lab1_AirportsTable/src/main/java/MyReducer.java
@@ -2,9 +2,12 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
 import java.util.Iterator;
 
 public class MyReducer extends Reducer <LongWritable, Text, LongWritable, Text>{
     @Override
-    protected void reduce (KeyIDnBase key, Iterable<Text> values, Context context)
+    protected void reduce (KeyIDnBase key, Iterable<Text> values, Context context) throws IOException,InterruptedException {
+        
+    }
 }
