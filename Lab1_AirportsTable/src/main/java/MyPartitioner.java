b45a155577d25db9cae8bac4f428fc3dc99d493b
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-01 19:24:18 +0300

    Scripted auto-commit on change (2019-11-01 19:24:18) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/MyPartitioner.java
+++ Lab1_AirportsTable/src/main/java/MyPartitioner.java
@@ -2,6 +2,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class MyPartitioner extends Partitioner <KeyIDnBase, Text>{
-        public int getPartition(K key, V value, int numReduceTasks) {
+        public int getPartition(KeyIDnBase key, Text value, int numReduceTasks) {
             return (KeyIDnBase.hashCode() & Integer.MAX_VALUE) % numReduceTasks;
         }
