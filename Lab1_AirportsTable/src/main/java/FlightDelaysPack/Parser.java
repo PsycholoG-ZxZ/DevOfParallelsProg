da4c585b2918c7880b2c62ec07535005af6e242b
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-02 02:11:31 +0300

    Scripted auto-commit on change (2019-11-02 02:11:31) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/FlightDelaysPack/Parser.java
+++ Lab1_AirportsTable/src/main/java/FlightDelaysPack/Parser.java
@@ -4,8 +4,8 @@ import org.apache.hadoop.io.Text;
 
 public class Parser {
     public static String[] parseIdDescr(Text str) {
-        //String[] data = str.toString().split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
-        String[] data = str.toString().split(",(?=\")");
+        String[] data = str.toString().split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+        //String[] data = str.toString().split(",(?=\")");
 
 
         return data;
