5174a0a70d9256dfba2b482f8727992b90155b17
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-01 20:17:15 +0300

    Scripted auto-commit on change (2019-11-01 20:17:15) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/MyReducer.java
+++ Lab1_AirportsTable/src/main/java/MyReducer.java
@@ -8,6 +8,6 @@ import java.util.Iterator;
 public class MyReducer extends Reducer <LongWritable, Text, LongWritable, Text>{
     @Override
     protected void reduce (KeyIDnBase key, Iterable<Text> values, Context context) throws IOException,InterruptedException {
-        Iterator
+        Iterator<Text>
     }
 }
