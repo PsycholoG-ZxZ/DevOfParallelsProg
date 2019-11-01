d6fb0f5ef67d28eef60afa18bacee230432b3b7c
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-01 19:59:43 +0300

    Scripted auto-commit on change (2019-11-01 19:59:43) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/FlightMapper.java
+++ Lab1_AirportsTable/src/main/java/FlightMapper.java
@@ -2,6 +2,15 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class FlightMapper extends Mapper<LongWritable, Text, KeyIDnBase, Text>  {
-    
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException,InterruptedException {
+        String data[] =Parser.parseIdDescr(value);
+        String id = data[0];
+        KeyIDnBase aKey = new KeyIDnBase(id, 0);
+        Text title = new Text(data[1]);
+        context.write(aKey,title);
+    }
 }
