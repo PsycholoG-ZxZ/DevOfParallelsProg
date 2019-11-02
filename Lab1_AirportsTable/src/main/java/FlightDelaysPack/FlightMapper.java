38358c9e7213b6202a27bd2e8ad952c20d7ad493
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-02 12:18:24 +0300

    Scripted auto-commit on change (2019-11-02 12:18:24) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/FlightDelaysPack/FlightMapper.java
+++ Lab1_AirportsTable/src/main/java/FlightDelaysPack/FlightMapper.java
@@ -11,6 +11,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, KeyIDnBase, Text>  
     protected void map(LongWritable key, Text value, Context context) throws IOException,InterruptedException {
         String data[] =Parser.parseSmall(value);
         if (data[17].equals("\"ARR_DELAY\"")) {return; }
+        if (data[17].equals("")) {return; }
         String id = data[14];
         KeyIDnBase aKey = new KeyIDnBase(id, 1);
         Text title = new Text(data[17]);
