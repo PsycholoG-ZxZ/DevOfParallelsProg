cbf66c59099faa057d38d18008d218f0ce787e89
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-08 18:41:14 +0300

    Scripted auto-commit on change (2019-11-08 18:41:13) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/FlightDelaysPack/GroupingComparator.java
+++ Lab1_AirportsTable/src/main/java/FlightDelaysPack/GroupingComparator.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.io.WritableComparator;
 
 public class GroupingComparator extends WritableComparator{
     protected GroupingComparator() {
-        super (KeyIDnBase.class, true);
+        super(KeyIDnBase.class, true);
     }
     @Override
     public int compare (WritableComparable ComparablePairFrst, WritableComparable ComparablePairSec){
