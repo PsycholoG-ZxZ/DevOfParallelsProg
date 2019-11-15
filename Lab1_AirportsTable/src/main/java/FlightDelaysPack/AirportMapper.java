28c6f380aa80b39cf16b546b0b5db9b80fc944cd
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-15 21:36:22 +0300

    Scripted auto-commit on change (2019-11-15 21:36:22) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/FlightDelaysPack/AirportMapper.java
+++ Lab1_AirportsTable/src/main/java/FlightDelaysPack/AirportMapper.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, KeyIDnBase, Text> {
 
-    private static int ID_AIR = 0 , DESCR = 1, SEC_TABLE_ID = 0;
+    private static int ID_AIR = 0 , DESCR = 1, FRST_TABLE_ID = 0;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,InterruptedException {
@@ -17,7 +17,7 @@ public class AirportMapper extends Mapper<LongWritable, Text, KeyIDnBase, Text> 
         if (CSVParser.CheckStringFrstCsv(id)) {
         return;
         }
-        KeyIDnBase aKey = new KeyIDnBase(CSVParser.RemoveSlash(id), SEC_TABLE_ID);
+        KeyIDnBase aKey = new KeyIDnBase(CSVParser.RemoveSlash(id), FRST_TABLE_ID);
         Text title = new Text(data[DESCR]);
         context.write(aKey,title);
 
