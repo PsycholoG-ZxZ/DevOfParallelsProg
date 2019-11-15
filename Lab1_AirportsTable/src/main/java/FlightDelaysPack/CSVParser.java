6ba9036af84c5a1f5c6b90d13b82948d7711c6e2
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-15 21:25:33 +0300

    Scripted auto-commit on change (2019-11-15 21:25:33) by gitwatch.sh

--- Lab1_AirportsTable/src/main/java/FlightDelaysPack/CSVParser.java
+++ Lab1_AirportsTable/src/main/java/FlightDelaysPack/CSVParser.java
@@ -2,6 +2,8 @@ package FlightDelaysPack;
 
 import org.apache.hadoop.io.Text;
 
+private static String FRST_TABLE="Code,Description";
+
 public class CSVParser {
     public static String[] parseIdDescr(Text str) {
         String[] data = str.toString().split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
@@ -10,9 +12,13 @@ public class CSVParser {
     public static String[] parseSmall(Text str) {
         String[] data = str.toString().split(",");
         return data;
+
     }
 
 
+    public static boolean CheckStringFrstCsv(Text str) {
+            return id.equals("Code,Description");
+    }
 
 
 
